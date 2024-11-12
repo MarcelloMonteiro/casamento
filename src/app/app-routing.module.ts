e704b8c7f2154ae7e56be5b379fddcca8c6784c1
João Marcelo <marcelodjmix2013@gmail.com>
2024-11-12 15:42:09 -0300

    commit inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Define HomeComponent como a rota inicial
+  { path: '**', redirectTo: '' } // Redireciona qualquer rota inválida para a página inicial
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
